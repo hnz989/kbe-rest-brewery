f77cc65beb7200e282eb194d5749526b768b5fc6
hnz989 <zhenghualin2008@gmail.com>
2022-07-15 09:01:30 -0400

    first commit

--- src/main/java/guru/springframework/sfgrestbrewery/KbeRestBrewery.java
+++ src/main/java/guru/springframework/sfgrestbrewery/KbeRestBrewery.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KbeRestBrewery {
-
+    //test
 	public static void main(String[] args) {
 		SpringApplication.run(KbeRestBrewery.class, args);
 	}
